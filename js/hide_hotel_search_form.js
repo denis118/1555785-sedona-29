a7732ccbdc3636fcab79f9c8816f36b4bfe790a3
Denis Tsap <ts.dios.v@gmail.com>
2020-10-23 21:55:29 +0300

    Доработка
    1. Сократил количество inline-block элементов
    2. У оставшихся inline-block элементов значение свойства vertical-align исправил на middle
    3. Привёл в соответствие требованию о переполнении контеном блоки .proposed-hotel
    4. Свёл к минимуму проявление субпиксельного рендеринга в Google Chrome в виде появления тёмной полосы на границе блоков .greeting и .outstanding-qualities при масштабировании страницы
    5. Убрал комментарии

--- js/hide_hotel_search_form.js
+++ js/hide_hotel_search_form.js
@@ -1,9 +1,9 @@
 'use strict'
 
-let hotelSearchButton = document.querySelector( '.button.hotel-search' );
+var hotelSearchButton = document.querySelector( '.button.hotel-search' );
 hotelSearchButton.addEventListener( 'click', openHide );
 
 function openHide () {
-  let form = document.querySelector( '.form-search' );
+  var form = document.querySelector( '.form-search' );
   form.classList.toggle( 'hide-mode' );
 }
